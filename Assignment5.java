3cd74ed77f3c12bd1973e620ff97b84bcb4f2d24
Richard Jean-Baptiste <Richinbk1@gmail.com>
2020-05-14 10:18:16 -0400

    Update Assignment5.java

--- Assignment5.java
+++ Assignment5.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 class BinaryTree  {
@@ -11,38 +13,56 @@ class BinaryTree  {
     class Node {
 
         String name;
+        String father;
         Node leftChild,rightChild;
 
         Node(String name){
             this.name = name;
+            father = "Not available";
             leftChild = null;
             rightChild = null;
+        }
+
+        Node(String name, String father){
+            this.name = name;
+            this.father = father;
+            leftChild = null;
+            rightChild = null;
+        }
+
+        public String getFather(){
+            return father;
         }
     }
 
     Node root;
     Node parent;
     Node focusNode;
-    String father;
 
-    public void add(Node node ,String name){
+    public void add(Node node ,String name,String parentName){
 
-        Node currNode = new Node(name);
+        try {
 
-        if(root == null){
-            root = currNode;
-            parent = root;
-            focusNode = root;
-            return;
-        }
+            Node currNode = new Node(name,parentName);
 
-        if(parent.leftChild == null){
-            parent.leftChild = currNode;
-            focusNode = parent.leftChild;
-        }else{
-            focusNode.rightChild = currNode;
-            focusNode = focusNode.rightChild;
-            return;
+            if(root == null){
+                root = currNode;
+                parent = root;
+                focusNode = root;
+                return;
+            }
+    
+            if(parent.leftChild == null){
+                parent.leftChild = currNode;
+                focusNode = parent.leftChild;
+            }else{
+                focusNode.rightChild = currNode;
+                focusNode = focusNode.rightChild;
+                return;
+            }
+            
+        } catch (NullPointerException e) {
+           
         }
 
     }
@@ -55,68 +75,127 @@ class BinaryTree  {
             root = childNode;
             parent = childNode;
         }else{
-
             node = getNode(root, name);
             parent = node;
             focusNode = parent;
-          
         }
 
     }
-    
+
+    public void getfather(String name){
+        try {
+            Node p = getNode(root, name);
+            System.out.println("The father of " + name + " is " + p.getFather());
+        } catch (Exception e) {
+            System.out.println(name + " couldn't get father");
+        }
+        
+    }
+ 
     public void getSons(String name){
 
-       Node p = getNode(root, name);
-       Node focus;
+        try {
 
-       if(p.leftChild == null){
-        System.out.println("no children");
-        return;
+            Node p = getNode(root, name);
+            Node focus;
+
+            if(p.leftChild == null){
+                System.out.println(name + " has no sons");
+                return;
+            }
+
+            System.out.print("The sons of " + name + " - ");
+
+            System.out.print(p.leftChild.name);
+            focus = p.leftChild;
+
+            while(focus.rightChild != null){
+                System.out.print(" " + focus.rightChild.name);
+                focus = focus.rightChild;
+            }
+
+            System.out.println(" ");
+            
+        } catch (NullPointerException e) {
+            System.out.println(name + " has no sons");
         }
-
-
-       System.out.print("The sons of " + name);
-       System.out.println(" ");
-
-       System.out.print(p.leftChild.name);
-       focus = p.leftChild;
-
-       while(focus.rightChild != null){
-            System.out.print(" " + focus.rightChild.name);
-            focus = focus.rightChild;
-       }
  
     }
 
-    public void getOldestSons(String name){
+    public void getBrothers(String name){
 
-        Node p = getNode(root, name);
+        try {
 
-        if(p.leftChild == null){
-            System.out.println("no children");
-            return;
+            String first = name;
+
+            Node p = getNode(root, name);
+
+            String x = p.getFather();
+
+            Node currFather = getNode(root, x);
+
+            Node focus;
+
+            System.out.print("The brothers of " + first + " - ");
+
+            System.out.print(currFather.leftChild.name);
+
+            focus = currFather.leftChild;
+
+            while(focus.rightChild != null){
+                System.out.print("  " + focus.rightChild.name);
+                focus = focus.rightChild;
+            }
+            
+        } catch (NullPointerException e) {
+            System.out.println(name + " has no brothers");
         }
 
-        System.out.println("The oldest son of " + name + " is " + p.leftChild.name);
+    }
+
+    public void getOldestSon(String name){
+
+        try {
+            Node p = getNode(root, name);
+
+            if(p.leftChild == null){
+                System.out.println(" ");
+                System.out.println(name + " has no children");
+                return;
+            }
+
+            System.out.println(" ");
+            System.out.println("The oldest son of " + name + " is " + p.leftChild.name);
+        } catch (NullPointerException e) {
+            System.out.println(name + " has no sons");
+        }
+
+        
     }
 
     public void getYoungestSon(String name){
 
-        Node p = getNode(root, name);
-        Node focus;
+        try {
 
-        if(p.leftChild == null){
-            System.out.println("no children");
-            return;
+            Node p = getNode(root, name);
+            Node focus;
+
+            if(p.leftChild == null){
+                System.out.println(name + " has no children");
+                return;
+            }
+
+            focus = p.leftChild;
+
+            while(focus.rightChild != null){
+                focus = focus.rightChild;
+            }
+
+            System.out.println("The youngest son of " + name + " is " + focus.name);
+            
+        } catch (NullPointerException e) {
+            System.out.println(name + " has no sons");
         }
-
-        focus = p.leftChild;
-
-        while(focus.rightChild != null){
-            focus = focus.rightChild;
-        }
-
-        System.out.println("The youngest son of " + name + " is " + focus.name);
 
     }
     
@@ -137,22 +216,22 @@ class BinaryTree  {
             return null;
         }
     }
-    
-    public void preorder(Node focusNode){
 
-        if(focusNode != null){
+    public void deleteTree() { 
+        root = null;
+    } 
 
-            System.out.println(focusNode.name);
-            preorder(focusNode.leftChild);
-            preorder(focusNode.rightChild);
-        }
+    static String randNames(ArrayList<String> x){
+
+        Random rand = new Random(); 
+        return x.get(rand.nextInt(x.size()));
     }
-
 
     public static void main(String[] args)throws FileNotFoundException, IOException {
         
         File data = new File("Data.txt");
         Scanner dataReader = new Scanner(new FileReader(data));
+        ArrayList<String> ranNames = new ArrayList<String>();
 
         BinaryTree familyTree = new BinaryTree();
     
@@ -164,30 +243,47 @@ class BinaryTree  {
         while(dataReader.hasNextLine()){
             String fullData = dataReader.nextLine();
             String[] dataLine = fullData.split(" ");
-            String name = dataLine[0];
-            count = Integer.parseInt(dataLine[1]);
 
-            familyTree.setParent(familyTree.root,name);
+            if(dataLine[0].equals("-999")){
 
-            for(int i = 0; i < count; i++){
-                line = Files.readAllLines(Paths.get("Data.txt")).get(lineNum + i);
-                String newLine[] = line.split(" ");
-
-                familyTree.add(familyTree.root,newLine[0]);
+                               
+                familyTree.getfather(randNames(ranNames));
+                System.out.println(" ");
+                familyTree.getSons(randNames(ranNames));
+                System.out.println(" ");
+                familyTree.getBrothers(randNames(ranNames));
+                System.out.println(" ");
+                familyTree.getOldestSon(randNames(ranNames));
+                System.out.println(" ");
+                familyTree.getYoungestSon(randNames(ranNames));
+                System.out.println("----------------------");
                 
+                lineNum += 2;
+                familyTree.deleteTree();
+                ranNames.clear();
+                
+            }else{
+
+                
+                String name = dataLine[0];
+                count = Integer.parseInt(dataLine[1]);
+                ranNames.add(name);
+
+                familyTree.setParent(familyTree.root,dataLine[0]);
+
+                for(int i = 0; i < count; i++){
+                    line = Files.readAllLines(Paths.get("Data.txt")).get(lineNum + i);
+                        String newLine[] = line.split(" ");
+                        familyTree.add(familyTree.root,newLine[0],name);  
+      
+                }
+           
+                lineNum += count;
+
             }
-
-            lineNum += count;
-
+                
         }
-
-        
-        //Node x = familyTree.getNode(familyTree.root, "Bob");
-        //System.out.println(x.leftChild.name);
-        
-        //System.out.println(familyTree.root.leftChild.leftChild.name);
-        //familyTree.preorder(familyTree.root);
-
+    
         dataReader.close();
         
     }
